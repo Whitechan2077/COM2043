9bbf7ee80ec40cc2c18fcec63ff106a583820662
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-25 17:07:41 +0700

    nah

--- Assigment_COM2043.sql
+++ Assigment_COM2043.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE Thong_Tin_Nha_Tro
+﻿CREATE DATABASE Thong_Tin_Nha_Tro
 ON(
 NAME = ThongTinNhaTro,
-FILENAME = 'D:\ThongTinNhaTro.mdf',
+FILENAME = 'D:\FPT POLYTECHNIC\Semester 3\SQL SERVER\COM2043\ASIGNMENT\ThongTinNhaTro.mdf',
 SIZE = 5,
 MAXSIZE = 20,
-GROWTH = 5
+FILEGROWTH = 5
 )
 LOG ON(
 NAME = ThongTinNhaTro_LOG,
-FILENAME = 'D:\ThongTinNhaTro.ldf',
+FILENAME = 'D:\FPT POLYTECHNIC\Semester 3\SQL SERVER\COM2043\ASIGNMENT\ThongTinNhaTro.ldf',
 SIZE = 5,
 MAXSIZE = 20,
-GROWTH = 5
+FILEGROWTH = 5
 );
 GO
 USE Thong_Tin_Nha_Tro;
@@ -21,9 +21,9 @@ CREATE TABLE Nguoi_Dung(
 	tenNguoiDung nvarchar(30) NOT NULL,
 	gioiTinh tinyint NOT NULL,
 	sdt nvarchar(10) NOT NULL UNIQUE,
-	diaChi nvarchar(50) CHECK(LEN(diaChi)>5) NOT NULL,
-	quan nvarchar(15) CHECK(LEN(quan)>3) NOT NULL,
-	email nvarchar(50) NOT NULL,
+	diaChi nvarchar(50) CHECK(LEN(diaChi)>1) NOT NULL,
+	quan nvarchar(15) CHECK(LEN(quan)>1) NOT NULL,
+	email nvarchar(50) NOT NULL
 )
 GO
 CREATE TABLE LOAI_NHA(
@@ -49,3 +49,11 @@ CREATE TABLE Danh_Gia(
 	noiDungDanhGia nvarchar(300)
 );
 -------------------------------------------------------
+CREATE PROCEDURE nhapNGuoiDung
+		@tenNguoiDung nvarchar(30),@gioiTinh tinyint,@sdt nvarchar(10),@diaChi nvarchar(50),@quan nvarchar(15),@email nvarchar(50)
+	AS
+	BEGIN
+	INSERT INTO Nguoi_Dung(tenNguoiDung,gioiTinh,sdt,diaChi,quan,email)
+		VALUES(@tenNguoiDung,@gioiTinh,@sdt,@diaChi,@quan,@email)
+	END
+EXEC nhapNGuoiDung N'Bùi Hoàng Dũng',0,'0397767819',N'Mỹ Đình 2',N'Nam Từ Liêm','buidung8198@gmail.com';
