f0428c92dd1584157e9a3d12d9d7706ecf05db4a
Hoàng Dũng <dungbui8198@gmail.com>
2023-05-30 16:53:06 +0700

    Lý thuyết

---
+++ trigger.sql
@@ -0,0 +1,11 @@
+﻿/*
+	Trigger là một dạng đặc biệt của store procedure
+	Tự thực hiên khi xảy ra ins up del
+	ko thể dùng exec để gọi và dùng 
+	là một dạng sp đặc biệt dùng kiểm tra sự toàn vẹn của dữ liệu 
+
+	2 loại trigger
+	1 DML ins del up định nghĩa ở DB
+	2 DDL create alter drop kiểm soát hoạt động của db ở múc cơ sở dữ liệu và sv
+
+*/
