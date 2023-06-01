4583618598f71d8312d7e446d705bf18b6373b11
Hoàng Dũng <dungbui8198@gmail.com>
2023-06-01 20:05:48 +0700

    Lý Thuyết

--- trigger.sql
+++ trigger.sql
@@ -1,19 +1,98 @@
-﻿/*
-	Trigger là một dạng đặc biệt của store procedure
-	Tự thực hiên khi xảy ra ins up del
-	ko thể dùng exec để gọi và dùng 
-	là một dạng sp đặc biệt dùng kiểm tra sự toàn vẹn của dữ liệu 
-
-	2 loại trigger
-	1 DML ins del up định nghĩa ở DB
-	2 DDL create alter drop kiểm soát hoạt động của db ở múc cơ sở dữ liệu và sv
-
-
-	--DML insert 
-	Sytax
-	CREATE TRIGGER ten ON bang FOR (INSERT,DELETE,UPDATE)
+﻿USE QLDA;
+GO
+ALTER TRIGGER XoaNv ON NHANVIEN
+	FOR DELETE 
 	AS
-	T-sql
-
-	--ROLLBACK TRANSACTION hoạt động của trigger để roll back lại thao tác với db 
+		BEGIN
+		DECLARE @DIACHI nvarchar(200);
+		SELECT @DIACHI = DCHI FROM NHANVIEN
+		IF (CHARINDEX('Tp HCM',@DIACHI)) > 0
+			BEGIN
+				PRINT 'Khong the xoa';
+				ROLLBACK TRANSACTION 
+			END
+		END
+DROP TRIGGER XoaNv
+DELETE FROM NHANVIEN WHERE MANV like '018';
+/*
+Cấu trúc của Tigger
+ CREATE TRIGGER tên_trigger ON tên_bảng 
+	FOR (INSERT , UPDATE <DELETE)
+	AS
+	t -sql
 */
+Go
+CREATE TRIGGER selectInsert ON NHANVIEN 
+	FOR INSERT
+	AS
+	BEGIN
+	SELECT * FROM NHANVIEN
+	END
+SELECT * FROM NHANVIEN
+INSERT INTO NHANVIEN
+		VALUES('A','B','C','018','2000-11-1',N'119 Cống Quỳnh, Tp HCM','Nam',50000,'006',5);
+GO
+CREATE TRIGGER tg_checkLuong ON NHANVIEN
+	FOR INSERT 
+	AS
+		BEGIN
+		IF (SELECT LUONG FROM inserted) < 5000 --ở trong bảng ảo
+		BEGIN
+			PRINT 'luong >5000'
+			ROLLBACK TRANSACTION 
+		END
+		END
+GO
+CREATE TRIGGER tg_check_update ON NHANVIEN 
+	FOR UPDATE 
+AS
+	BEGIN
+	IF(SELECT LUONG FROM inserted) <5000
+		BEGIN
+		PRINT 'Luong >5000'
+		ROLLBACK TRANSACTION
+		END
+	END
+	UPDATE NHANVIEN 
+		SET LUONG = 5000
+		WHERE MANV like '018'
+/*
+Nói một phần về lý thuyết
+trigger sinh ra để kiểm tra dữ liệu của db khi được thay đổi
+Trigger có 2 DML DDL 
+-- AFTER TRIGGER 
+Trigger đã được dùng sau khi tương tác xong với csdl
+*/
+GO
+CREATE TRIGGER tg_checkNv 
+	ON NHANVIEN
+	AFTER DELETE 
+	AS
+		BEGIN
+		SELECT COUNT(MANV) AS'Da xoa nhan vien' FROM deleted
+	END
+GO
+CREATE TRIGGER Tg_checkUpdate 
+	ON NHANVIEN
+	AFTER UPDATE
+	AS
+		BEGIN
+		SELECT COUNT(MANV) FROM inserted
+		END
+/*
+INSTEAD OF TRIGGER
+ Khi delete ,UPDATE INSERT => xảy ra cấn do rằng buộc
+ => sinh ra để giải quyết, change toàn bộ
+ vd delete sẽ delete dữ liệu liên quan
+*/
+GO
+CREATE TRIGGER tg_delete ON NHANVIEN
+	INSTEAD OF DELETE 
+	AS
+	BEGIN
+		DELETE FROM THANNHAN WHERE MA_NVIEN IN (SELECT MANV FROM deleted)
+		DELETE FROM NHANVIEN WHERE MANV IN (SELECT MANV FROM deleted)
+	END
+DELETE FROM NHANVIEN WHERE MANV = '017'
+SELECT * FROM THANNHAN
+SELECT * FROM NHANVIEN
